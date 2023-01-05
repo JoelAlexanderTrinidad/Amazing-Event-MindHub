739f253d4a127a165c0237ad752ab50d36e21ffa
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-01-05 14:20:39 -0300

    task-3 done

--- js/upComingEvents.js
+++ js/upComingEvents.js
@@ -19,18 +19,18 @@ function cardsUpComing(arr, lugar){
     let div = document.createElement('div')
     div.classList.add('row', 'row-cols-2', 'container-fluid', 'px-0', 'mx-auto', 'justify-content-center', 'justify-content-lg-evenly', 'gap-3', 'my-4')
 
-    for(let elemento of arr){
+    for(let evento of arr){
         div.innerHTML += `
             <div class="card col-5 col-lg-3 col-xl-2">
                 <div class="p-2 pt-md-3 tarjeta">
-                    <img src=${elemento.image} class="img-tarjeta" alt="...">
+                    <img src=${evento.image} class="img-tarjeta" alt="...">
                     <div class="">
-                        <h5 class="pt-1 titulo-tarjeta">${elemento.name}</h5>
-                        <p class="texto-tarjeta">${elemento.description.slice(0,40)}...</p>
-                        <p class="text-center pt-2 mb-0">Price: <span class="text-success">USD ${elemento.price}</span></p>
-                        <p class="texto-tarjeta">Date: ${elemento.date}</p>
+                        <h5 class="pt-1 titulo-tarjeta">${evento.name}</h5>
+                        <p class="texto-tarjeta">${evento.description.slice(0,40)}...</p>
+                        <p class="text-center pt-2 mb-0">Price: <span class="text-success">USD ${evento.price}</span></p>
+                        <p class="texto-tarjeta">Date: ${evento.date}</p>
                         <div class="justify-content-center d-flex">
-                            <a href="./details.html" class="btn btn-danger boton-tarjeta">View more</a>
+                            <a href="./details.html?id=${evento._id}" class="btn btn-danger boton-tarjeta">View more</a>
                         </div>
                     </div>
                 </div>
